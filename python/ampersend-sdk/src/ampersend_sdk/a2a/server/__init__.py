56a6aa4956a4054e854cfeec0b9482a635cfced7
Matias <matias@edgeandnode.com>
2025-10-22 20:39:36 -0300

    feat: add examples

--- python/ampersend-sdk/src/ampersend_sdk/a2a/server/__init__.py
+++ python/ampersend-sdk/src/ampersend_sdk/a2a/server/__init__.py
@@ -1,13 +1,13 @@
 from .a2a_executor import (
-    InnerA2aAgentExecutor,
-    OuterA2aAgentExecutor,
     X402A2aAgentExecutor,
 )
+from .before_agent_callback import make_x402_before_agent_callback
+from .to_a2a import to_a2a
 from .x402_server_executor import X402ServerExecutor
 
 __all__ = [
-    "InnerA2aAgentExecutor",
-    "OuterA2aAgentExecutor",
+    "make_x402_before_agent_callback",
+    "to_a2a",
     "X402A2aAgentExecutor",
     "X402ServerExecutor",
 ]
