351ce4b98cf9a3d2d84bd46b8e701cc128bf0c08
Matias <matias@edgeandnode.com>
2026-02-27 14:26:34 -0300

    refactor(sdk): import OWNABLE_VALIDATOR_ADDRESS from Rhinestone SDK

--- typescript/packages/ampersend-sdk/src/smart-account/constants.ts
+++ typescript/packages/ampersend-sdk/src/smart-account/constants.ts
@@ -2,8 +2,10 @@
  * Smart account constants
  */
 
+import { OWNABLE_VALIDATOR_ADDRESS } from "@rhinestone/module-sdk"
+
 /**
  * OwnableValidator address - ERC-7579 validator for smart accounts
  * This is the standard validator used by most smart account implementations
  */
-export const OWNABLE_VALIDATOR = "0x000000000013fdB5234E4E3162a810F54d9f7E98" as const
+export const OWNABLE_VALIDATOR = OWNABLE_VALIDATOR_ADDRESS
